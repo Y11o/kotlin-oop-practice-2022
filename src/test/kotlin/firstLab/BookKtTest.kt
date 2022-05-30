53bc6b2d6d192dd1a27d0ddb7f5cb2474b69c8ca
Y11o <shakhmardanov.ilnur@mail.ru>
2022-05-30 12:35:36 +0400

    Lab1Tests

--- src/test/kotlin/firstLab/BookKtTest.kt
+++ src/test/kotlin/firstLab/BookKtTest.kt
@@ -12,13 +12,13 @@ internal class BookKtTest{
                 "3.1984//G. Orwell//1949 "
         val bookList:List<Book> = parseBooks(books)
         assertEquals(bookList[0].title, "Crime and Punishment")
-        assertEquals(bookList[0].author, "F.M. Dostoevsky")
+        assertEquals(bookList[0].authors[0], "F.M. Dostoevsky")
         assertEquals(bookList[0].publicationDate, 1866)
         assertEquals(bookList[1].title, "The Master and Margarita")
-        assertEquals(bookList[1].author, "M.A. Bulgakov")
+        assertEquals(bookList[1].authors[0], "M.A. Bulgakov")
         assertEquals(bookList[1].publicationDate, 1966)
         assertEquals(bookList[2].title, "1984")
-        assertEquals(bookList[2].author, "G. Orwell")
+        assertEquals(bookList[2].authors[0], "G. Orwell")
         assertEquals(bookList[2].publicationDate, 1949)
     }
 }
